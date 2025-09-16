bad29a64b3d9c84538c2f2d5af44b2e18340ef4a
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-09-16 22:40:01 +0500

    Initial commit of simplex-chat-3rd project

--- src/Simplex/Chat/Core.hs
+++ src/Simplex/Chat/Core.hs
@@ -107,7 +107,7 @@ createActiveUser cc = do
   where
     loop = do
       displayName <- T.pack <$> getWithPrompt "display name"
-      let profile = Just Profile {displayName, fullName = "", image = Nothing, contactLink = Nothing, preferences = Nothing}
+      let profile = Just Profile {displayName, fullName = "", image = Nothing, contactLink = Nothing, preferences = Nothing, defaultTimerTTL = Nothing}
       execChatCommand' (CreateActiveUser NewUser {profile, pastTimestamp = False}) `runReaderT` cc >>= \case
         Right (CRActiveUser user) -> pure user
         r -> printResponseEvent (Nothing, Nothing) (config cc) r >> loop
