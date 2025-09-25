e0a33b1ca91f11345a552d807d7aeb7a39b029b4
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-09-25 23:21:07 +0500

    Initial commit of simplex-chat-3rd project

--- src/Simplex/Chat/Library/Subscriber.hs
+++ src/Simplex/Chat/Library/Subscriber.hs
@@ -2049,11 +2049,9 @@ processAgentMessageConn vr user corrId agentConnId agentMessage = do
     processContactProfileUpdate c@Contact {profile = lp, contactId = ctId, activeConn, chatItemTTL = oldChatItemTTL} p' createItems
       | p /= p' = do
           -- Check if this is initial connection (no chatItemTTL) and timer negotiation is needed
-          let contactDefaultTimerTTL = (defaultTimerTTL :: Profile -> Maybe Int64) p'
-              Profile {preferences = contactPreferences} = p'
+          let Profile {defaultTimerTTL = contactDefaultTimerTTL, preferences = contactPreferences} = p'
               needsTimerNegotiation = isNothing oldChatItemTTL && isJust contactDefaultTimerTTL
-              userDefaultTTL = (defaultTimerTTL :: User -> Int64) user
-              User {userId} = user
+              User {defaultTimerTTL = userDefaultTTL, userId} = user
           
           (c', timedDeleteAtList, profileUpdateNeeded) <- withStore $ \db -> do
             if needsTimerNegotiation
@@ -2111,7 +2109,8 @@ processAgentMessageConn vr user corrId agentConnId agentMessage = do
           
           -- Emit timer negotiation event if this was an initial timer negotiation
           when profileUpdateNeeded $ do
-            case (chatItemTTL :: Contact -> Maybe Int64) c' of
+            let Contact {chatItemTTL = cTTL} = c'
+            case cTTL of
               Just ttl -> toView $ CEvtTimerNegotiated user c' ttl
               Nothing -> pure ()
           
@@ -2467,11 +2466,13 @@ processAgentMessageConn vr user corrId agentConnId agentMessage = do
           ct <- withStore $ \db -> createDirectContact db user conn' p
           toView $ CEvtContactConnecting user ct
           -- Emit timer negotiation event if a timer was negotiated
-          case ((defaultTimerTTL :: User -> Int64) user, (defaultTimerTTL :: Profile -> Maybe Int64) p) of
-            (uTTL, Just cTTL) -> do
-              let negotiatedTTL = min uTTL cTTL
+          let User {defaultTimerTTL = uTTL} = user
+              Profile {defaultTimerTTL = pTTL} = p
+          case (uTTL, pTTL) of
+            (uTTL', Just cTTL) -> do
+              let negotiatedTTL = min uTTL' cTTL
               toView $ CEvtTimerNegotiated user ct negotiatedTTL
-            (uTTL, Nothing) -> toView $ CEvtTimerNegotiated user ct uTTL
+            (uTTL', Nothing) -> toView $ CEvtTimerNegotiated user ct uTTL'
           pure (conn', False)
         XGrpLinkInv glInv -> do
           (gInfo, host) <- withStore $ \db -> createGroupInvitedViaLink db vr user conn' glInv
